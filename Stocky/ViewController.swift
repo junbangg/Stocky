f7babdbf72be51624639b0fc8253673a1cd8c74c
junbangg <wnstjr4620@naver.com>
2021-01-13 20:55:01 +0900

    Initial Commit

---
+++ Stocky/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Stocky
+//
+//  Created by Jun suk Bang on 2021/01/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
