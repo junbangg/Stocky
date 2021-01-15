6ce303b8b5cea30db41771cf013e3f0ac7b3df12
junbangg <wnstjr4620@naver.com>
2021-01-15 23:42:46 +0900

    table view dynamic

--- Stocky/Controller/SearchTableViewController.swift
+++ Stocky/Controller/SearchTableViewController.swift
@@ -24,7 +24,9 @@ class SearchTableViewController: UITableViewController {
     
     private let apiService = APIService()
     private var subscribers = Set<AnyCancellable>()
+    private var searchResults : SearchResults?
     @Published var searchQuery = String()
+    
     
     
     override func viewDidLoad() {
@@ -50,32 +52,25 @@ class SearchTableViewController: UITableViewController {
                         break
                     }
                 } receiveValue: { (searchResults) in
-                    print(searchResults)
+                    self.searchResults = searchResults
+                    self.tableView.reloadData()
                 }.store(in: &self.subscribers)
 
             }.store(in: &subscribers)
         
     }
     
-//    private func performSearch() {
-//        apiService.fetchSymbolsPublisher(key: "S&P500").sink { (completion) in
-//            switch completion {
-//            case .failure(let error):
-//                print(error.localizedDescription)
-//            case .finished: break
-//            }
-//        } receiveValue: { (searchResults) in
-//            print(searchResults)
-//        }.store(in: &subscribers)
-//
-//    }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
+        return searchResults?.items.count ?? 0
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cellID", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cellID", for: indexPath) as! SearchTableViewCell
+        if let searchResults = self.searchResults {
+            let searchResult = searchResults.items[indexPath.row]
+            cell.configure(with: searchResult)
+        }
         return cell
     }
 
