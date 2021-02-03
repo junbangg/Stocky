793f6517f6610802657d76a858595cc1d6e2d1bc
junbangg <wnstjr4620@naver.com>
2021-02-03 18:51:11 +0900

    fixed UI bug(label)

--- Stocky/Controller/CalculatorTableViewController.swift
+++ Stocky/Controller/CalculatorTableViewController.swift
@@ -13,6 +13,8 @@ class CalculatorTableViewController: UITableViewController {
     @IBOutlet weak var symbolLabel : UILabel!
     @IBOutlet weak var assetLabel : UILabel!
     @IBOutlet var currencyLabels: [UILabel]!
+    @IBOutlet weak var investmentAmountCurrencyLabel : UILabel!
+    
     
     var asset : Asset?
     override func viewDidLoad() {
@@ -23,6 +25,7 @@ class CalculatorTableViewController: UITableViewController {
     private func setupViews() {
         symbolLabel.text = asset?.searchResult.symbol
         assetLabel.text = asset?.searchResult.name
+        investmentAmountCurrencyLabel.text = asset?.searchResult.currency
         currencyLabels.forEach { (label) in
             label.text = asset?.searchResult.currency.addBrackets()
         }
