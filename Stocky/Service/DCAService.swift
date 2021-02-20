548887cf348f29e955f18df01b43383a3abbebb4
junbangg <wnstjr4620@naver.com>
2021-02-20 23:38:06 +0900

    Get Investment amount implemented

--- Stocky/Service/DCAService.swift
+++ Stocky/Service/DCAService.swift
@@ -11,11 +11,23 @@ struct DCAService {
     func calculate(initialInvestmentAmount: Double,
                    monthlyDollarCostAveragingAmount: Double,
                    initialDateOfInvestmentIndex: Int) -> DCAResult{
+        
+        let investmentAmount = getInvestmentAmount(initialInvestmentAmount: initialInvestmentAmount,              monthlyDollarCostAveragingAmount: monthlyDollarCostAveragingAmount,
+                                                   initialDateOfInvestmentIndex: initialDateOfInvestmentIndex)
         return .init(currentValue: 0,
-                     investmentAmount: 0,
+                     investmentAmount: investmentAmount,
                      gain: 0,
                      yield: 0,
                      annualReturn: 0)
+    }
+    private func getInvestmentAmount(initialInvestmentAmount: Double,
+                                     monthlyDollarCostAveragingAmount: Double,
+                                     initialDateOfInvestmentIndex: Int) -> Double{
+        var totalAmount = Double()
+        totalAmount += initialInvestmentAmount
+        let dollarCostAveragingAmount = initialDateOfInvestmentIndex.doubleValue * monthlyDollarCostAveragingAmount
+        totalAmount += dollarCostAveragingAmount
+        return totalAmount
     }
 }
 
