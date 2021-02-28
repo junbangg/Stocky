588e005f81a0dec13d4b436e4ffff89ba9f77d74
junbangg <wnstjr4620@naver.com>
2021-03-01 00:12:32 +0900

    Added Unit Test

--- StockyTests/StockyTests.swift
+++ StockyTests/StockyTests.swift
@@ -9,7 +9,7 @@ import XCTest
 @testable import Stocky
 
 class StockyTests: XCTestCase {
-
+    
     var sut: DCAService!
     
     override func setUpWithError() throws {
@@ -17,24 +17,86 @@ class StockyTests: XCTestCase {
         try super.setUpWithError()
         sut = DCAService()
     }
-
+    
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
         try super.tearDownWithError()
         sut = nil
     }
     
-    //Format for test function name
-    //What
-    //Given
-    //Expected
-    func testDCAResult_givenDCAIsUsed_expecteResult() {
+    /// Test Cases
+    //    1. asset = winning | dca = true => positive gains
+    //    2. asset = winning | dca = false => positive gains
+    //    3. asset = losing | dca = true => negative gains
+    //    4. asset = losing | dca = false => negative gains
+    
+    
+    /// Format for test function name
+    //  What
+    //  Given
+    //  Expected
+    
+    /// Format for test function
+    //  Given
+    //  When
+    //  Then
+    
+    func testResult_givenWinningAssetAndDCAIsUsed_expectPositiveGains() {
+        //given
+        let initialInvestmentAmount : Double = 5000
+        let monthlyDollarCostAveragingAmount : Double = 1500
+        let initialDateOfInvestmentIndex : Int = 5
+        let asset = buildWinningAsset()
+        //when
+        let result = sut.calculate(asset: asset,
+                                 initialInvestmentAmount: initialInvestmentAmount,
+                                 monthlyDollarCostAveragingAmount: monthlyDollarCostAveragingAmount,
+                                 initialDateOfInvestmentIndex: initialDateOfInvestmentIndex)
+        //then
+        // Initial Investment : $5000
+        // DCA : $1500 x 5 = $7500
+        // total : $5000 + $7500 = $125000
+        //
+        XCTAssertEqual(result.investmentAmount, 12500)
+        
+        
+    }
+    func testResult_givenWinningAssetAndDCAIsNotUsed_expectPositiveGains() {
+        
+    }
+    func testResult_givenLosingAssetAndDCAIsUsed_expectNegativeGains() {
+        
+    }
+    func testResult_givenLosingAssetAndDCAIsNotUsed_expectNegative() {
         
     }
     
-    func testDCAResult_givenDCAIsNotUsed_expectResult() {
+    private func buildWinningAsset() -> Asset {
+        let searchResult = buildSearchResult()
+        let meta = buildMeta()
+        let timeSeries : [String : TSData] = ["2008-02-29": TSData(open: "100", close: "0", adjustedClose: "110"),
+                                            "2008-03-29": TSData(open: "110", close: "0", adjustedClose: "120"),
+                                            "2008-04-29": TSData(open: "120", close: "0", adjustedClose: "130"),
+                                            "2008-05-29": TSData(open: "130", close: "0", adjustedClose: "140"),
+                                            "2008-06-29": TSData(open: "140", close: "0", adjustedClose: "150"),
+                                            "2008-07-29": TSData(open: "150", close: "0", adjustedClose: "160")]
+        let timeSeriesMonthlyAdjusted  = TimeSeries(meta: meta, timeSeries: timeSeries)
         
+        return Asset(searchResult: searchResult, timeSeries: timeSeriesMonthlyAdjusted)
     }
+    
+    private func buildSearchResult() -> SearchResult {
+        return SearchResult(symbol: "XYZ", name: "XYZCompany", type: "ETF", currency: "USD")
+    }
+    
+    private func buildMeta() -> Meta {
+        return Meta(symbol: "XYZ")
+    }
+    
+    
+    
+    
+    
     
     func testInvestmentAmount_whenDCAIsUsed_expectResult() {
         // given
@@ -67,5 +129,5 @@ class StockyTests: XCTestCase {
         // total: $0 + $500 = $500
         
     }
-
+    
 }
