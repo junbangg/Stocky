962524b2385b0656d7ec04de792e94d7ad4bb4b5
junbangg <wnstjr4620@naver.com>
2021-03-01 23:05:58 +0900

    Added Final Unit Test

--- StockyTests/StockyTests.swift
+++ StockyTests/StockyTests.swift
@@ -49,9 +49,9 @@ class StockyTests: XCTestCase {
         let asset = buildWinningAsset()
         //when
         let result = sut.calculate(asset: asset,
-                                 initialInvestmentAmount: initialInvestmentAmount,
-                                 monthlyDollarCostAveragingAmount: monthlyDollarCostAveragingAmount,
-                                 initialDateOfInvestmentIndex: initialDateOfInvestmentIndex)
+                                   initialInvestmentAmount: initialInvestmentAmount,
+                                   monthlyDollarCostAveragingAmount: monthlyDollarCostAveragingAmount,
+                                   initialDateOfInvestmentIndex: initialDateOfInvestmentIndex)
         //then
         // Initial Investment : $5000
         // DCA : $1500 x 5 = $7500
@@ -82,9 +82,9 @@ class StockyTests: XCTestCase {
         let asset = buildWinningAsset()
         //when
         let result = sut.calculate(asset: asset,
-                                 initialInvestmentAmount: initialInvestmentAmount,
-                                 monthlyDollarCostAveragingAmount: monthlyDollarCostAveragingAmount,
-                                 initialDateOfInvestmentIndex: initialDateOfInvestmentIndex)
+                                   initialInvestmentAmount: initialInvestmentAmount,
+                                   monthlyDollarCostAveragingAmount: monthlyDollarCostAveragingAmount,
+                                   initialDateOfInvestmentIndex: initialDateOfInvestmentIndex)
         //then
         // Initial Investment : $5000
         // DCA : $0 x 3 = $0
@@ -102,7 +102,7 @@ class StockyTests: XCTestCase {
         // Total current value = 41.6666 * 160 (latest month closing price) = $6666.666
         
         /// Gain Formula
-        //Gain = Current Value - Initial Investment = 6666.666 - 5000
+        //Gain = Current Value - Initial Investment = 6666.666 - 5000 = 1666.666
         
         /// Yield Formula
         // Yield = Gain / Investment Amount = 1666.666 / 5000
@@ -112,30 +112,107 @@ class StockyTests: XCTestCase {
         
     }
     func testResult_givenLosingAssetAndDCAIsUsed_expectNegativeGains() {
+        //given
+        let initialInvestmentAmount : Double = 5000
+        let monthlyDollarCostAveragingAmount : Double = 1500
+        let initialDateOfInvestmentIndex : Int = 5
+        let asset = buildLosingAsset()
+        //when
+        let result = sut.calculate(asset: asset,
+                                   initialInvestmentAmount: initialInvestmentAmount,
+                                   monthlyDollarCostAveragingAmount: monthlyDollarCostAveragingAmount,
+                                   initialDateOfInvestmentIndex: initialDateOfInvestmentIndex)
+        //then
+        // Initial Investment : $5000
+        // DCA : $1500 x 5 = $7500
+        // total : $5000 + $7500 = $125000
+        //
+        XCTAssertEqual(result.investmentAmount, 12500)
+        XCTAssertFalse(result.isProtiable)
+        /// Current Value Formula
+        // Jan: $5000 / 170 = 29.4117 shares
+        // Feb: $1500 / 160 = 9.375 shares
+        // Mar: $1500 / 150 = 10 shares
+        // April: $1500 / 140 = 10.7142 shares
+        // May: $1500 / 140 = 11.5384 shares
+        // June: $1500 / 120 = 12.5 shares
+        // Total Shares = 83.5393 shares
+        // Total current value = 83.5393 * 110 (latest month closing price) = $9189.323
+        /// Gain Formula
+        // Gains = current value - invesment amount = 9189.323 - 12500 = -3310.677
+        /// Yield Formula
+        // Yield = gains / investment amount : -3310.677 / 12500 = -0.2648
         
+        
+        XCTAssertEqual(result.currentValue, 9189.323, accuracy: 0.1)
+        XCTAssertEqual(result.gain, -3310.677, accuracy: 0.1)
+        XCTAssertEqual(result.yield, -0.2648, accuracy: 0.0001)
     }
     func testResult_givenLosingAssetAndDCAIsNotUsed_expectNegative() {
+        //given
+        let initialInvestmentAmount : Double = 5000
+        let monthlyDollarCostAveragingAmount : Double = 0
+        let initialDateOfInvestmentIndex : Int = 3
+        let asset = buildLosingAsset()
+        //when
+        let result = sut.calculate(asset: asset,
+                                   initialInvestmentAmount: initialInvestmentAmount,
+                                   monthlyDollarCostAveragingAmount: monthlyDollarCostAveragingAmount,
+                                   initialDateOfInvestmentIndex: initialDateOfInvestmentIndex)
+        //then
+        // Initial Investment : $5000
+        // DCA : $0 x 3 = $0
+        // total : $5000 + $0 = $5000
+        //
+        XCTAssertEqual(result.investmentAmount, 5000)
+        XCTAssertFalse(result.isProtiable)
+        /// Current Value Formula
+        // Mar: $5000 / 150 = 33.3333 shares
+        // April: $0 / 140 = 0 shares
+        // May: $0 / 130 = 0 shares
+        // June: $0 / 120 = 0 shares
+        // Total Shares = 33.3333 shares
+        // Total current value = 33.3333 * $110 (latest month closing price) = $3666.6666
+        /// Gain Formula
+        // Gains = current value - invesment amount = $3666.6666 - 5000 = -1333.333
+        /// Yield Formula
+        // Yield = gains / investment amount : -1333.333 / 5000 = -0.26666
         
+        
+        XCTAssertEqual(result.currentValue, 3666.666, accuracy: 0.1)
+        XCTAssertEqual(result.gain, -1333.333, accuracy: 0.1)
+        XCTAssertEqual(result.yield, -0.26666, accuracy: 0.0001)
     }
     
     private func buildWinningAsset() -> Asset {
         let searchResult = buildSearchResult()
         let meta = buildMeta()
-        let timeSeries : [String : TSData] = ["2008-02-29": TSData(open: "100", close: "110", adjustedClose: "110"),
-                                            "2008-03-29": TSData(open: "110", close: "120", adjustedClose: "120"),
-                                            "2008-04-29": TSData(open: "120", close: "130", adjustedClose: "130"),
-                                            "2008-05-29": TSData(open: "130", close: "140", adjustedClose: "140"),
-                                            "2008-06-29": TSData(open: "140", close: "150", adjustedClose: "150"),
-                                            "2008-07-29": TSData(open: "150", close: "160", adjustedClose: "160")]
-       
+        let timeSeries : [String : TSData] = ["2008-01-29": TSData(open: "100", close: "110", adjustedClose: "110"),
+                                              "2008-02-29": TSData(open: "110", close: "120", adjustedClose: "120"),
+                                              "2008-03-29": TSData(open: "120", close: "130", adjustedClose: "130"),
+                                              "2008-04-29": TSData(open: "130", close: "140", adjustedClose: "140"),
+                                              "2008-05-29": TSData(open: "140", close: "150", adjustedClose: "150"),
+                                              "2008-06-29": TSData(open: "150", close: "160", adjustedClose: "160")]
+        
         let timeSeriesMonthlyAdjusted  = TimeSeries(meta: meta, timeSeries: timeSeries)
         
         return Asset(searchResult: searchResult, timeSeries: timeSeriesMonthlyAdjusted)
     }
     
-//    private func buildLosingAsset() -> Asset {
-//
-//    }
+    private func buildLosingAsset() -> Asset {
+        let searchResult = buildSearchResult()
+        let meta = buildMeta()
+        let timeSeries : [String : TSData] = ["2021-01-25": TSData(open: "170", close: "160", adjustedClose: "160"),
+                                              "2021-02-25": TSData(open: "160", close: "150", adjustedClose: "150"),
+                                              "2021-03-25": TSData(open: "150", close: "140", adjustedClose: "140"),
+                                              "2021-04-25": TSData(open: "140", close: "130", adjustedClose: "130"),
+                                              "2021-05-25": TSData(open: "130", close: "120", adjustedClose: "120"),
+                                              "2021-06-25": TSData(open: "120", close: "110", adjustedClose: "110")]
+        
+        let timeSeriesMonthlyAdjusted  = TimeSeries(meta: meta, timeSeries: timeSeries)
+        
+        return Asset(searchResult: searchResult, timeSeries: timeSeriesMonthlyAdjusted)
+    }
     
     private func buildSearchResult() -> SearchResult {
         return SearchResult(symbol: "XYZ", name: "XYZCompany", type: "ETF", currency: "USD")
@@ -144,9 +221,6 @@ class StockyTests: XCTestCase {
     private func buildMeta() -> Meta {
         return Meta(symbol: "XYZ")
     }
-    
-    
-    
     
     
     
