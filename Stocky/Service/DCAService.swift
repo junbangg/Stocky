23083a2f84842d9b3f593329bcb90e0d69e354d5
junbangg <wnstjr4620@naver.com>
2021-04-14 19:31:33 +0900

    Documentation for DCAService

--- Stocky/Service/DCAService.swift
+++ Stocky/Service/DCAService.swift
@@ -6,8 +6,26 @@
 //
 
 import Foundation
-
+/**
+ Struct used to calculate Dollar Cost Averaging
+ 
+ # Components
+ - func calculate(asset: Asset, initialInvestmentAmount: Double, monthlyDollarCostAveragingAmount : Double, initialDateOfInvestment) -> DCAResult
+ - func getInvestmentAmount(initialInvestmentAmount: Double, monthlyDollarCostAveragingAmount: Double, initialDateOfInvestmentIndex: Int) -> Double
+ - private func getAnnualReturn(currentValue: Double, investmentAmount: Double, initialDateOfInvestmentIndex: Int) -> Double
+ - private func getCurrentValue(numberOfShares : Double, latestSharePrice : Double) -> Double
+ - private func getLatestSharedPrice(asset : Asset) -> Double
+ - private func getLatestSharedPrice(asset : Asset) -> Double
+ - private func getNumberOfShares(asset: Asset, initialInvestmentAmount: Double, monthlyDollarCostAveragingAmount: Double, initialDateOfInvestmentIndex: Int) -> Double
+ */
 struct DCAService {
+    /// Function to calculate the DCA
+    /// - Parameters:
+    ///   - asset: Asset that holds the data needed for calculation
+    ///   - initialInvestmentAmount: initial investment provided by user
+    ///   - monthlyDollarCostAveragingAmount: monthly dollar cost averaging  provided by user
+    ///   - initialDateOfInvestmentIndex: initial date of investment provided by user converted to index
+    /// - Returns: DCAResult with data to present in view
     func calculate(asset: Asset,
                    initialInvestmentAmount: Double,
                    monthlyDollarCostAveragingAmount: Double,
@@ -37,9 +55,15 @@ struct DCAService {
                      isProtiable: isProfitable)
     }
     
+    /// Function to calculate investment amount
+    /// - Parameters:
+    ///   - initialInvestmentAmount: initial investment provided by user
+    ///   - monthlyDollarCostAveragingAmount: monthly dollar cost averaging  provided by user
+    ///   - initialDateOfInvestmentIndex: initial date of investment provided by user converted to index
+    /// - Returns: Investment amount
     func getInvestmentAmount(initialInvestmentAmount: Double,
                                      monthlyDollarCostAveragingAmount: Double,
-                                     initialDateOfInvestmentIndex: Int) -> Double{
+                                     initialDateOfInvestmentIndex: Int) -> Double {
         var totalAmount = Double()
         totalAmount += initialInvestmentAmount
         let dollarCostAveragingAmount = initialDateOfInvestmentIndex.doubleValue * monthlyDollarCostAveragingAmount
@@ -47,6 +71,12 @@ struct DCAService {
         return totalAmount
     }
     
+    /// Function to calculate annual return
+    /// - Parameters:
+    ///   - currentValue: current value of investment
+    ///   - investmentAmount: result of getInvestmentAmount()
+    ///   - initialDateOfInvestmentIndex: initial date of investment provided by user converted to index
+    /// - Returns: Annual return
     private func getAnnualReturn(currentValue: Double, investmentAmount: Double, initialDateOfInvestmentIndex: Int) -> Double {
         let rate = currentValue / investmentAmount
         let years = (initialDateOfInvestmentIndex.doubleValue + 1) / 12
@@ -54,14 +84,29 @@ struct DCAService {
         return result
     }
     
+    /// Function to calculate current value of investment
+    /// - Parameters:
+    ///   - numberOfShares: number of shares
+    ///   - latestSharePrice: lastest share price
+    /// - Returns: Current Value of investment
     private func getCurrentValue(numberOfShares : Double, latestSharePrice : Double) -> Double {
         return numberOfShares * latestSharePrice
     }
     
+    /// Function to calculate latest shared price
+    /// - Parameter asset: Asset that holds all data required for calculation
+    /// - Returns: Extracts latest shared price data
     private func getLatestSharedPrice(asset : Asset) -> Double {
         return asset.timeSeries.getMonthData(dateReverseSort: true).first?.adjustedClose ?? 0
     }
     
+    /// Function to extract number of shares
+    /// - Parameters:
+    ///   - asset: Asset that holds all data required for calculation
+    ///   - initialInvestmentAmount: initial investment provided by user
+    ///   - monthlyDollarCostAveragingAmount: monthly dollar cost averaging  provided by user
+    ///   - initialDateOfInvestmentIndex: initial date of investment provided by user converted to index
+    /// - Returns: Total amount of shares
     private func getNumberOfShares(asset: Asset,
                                    initialInvestmentAmount: Double,
                                    monthlyDollarCostAveragingAmount: Double,
@@ -81,6 +126,7 @@ struct DCAService {
 }
 
 
+/// Result type of Dollar Cost Averaging 
 struct DCAResult {
     let currentValue : Double
     let investmentAmount : Double
