be7a7ccf24547b41c8cb3cd2b9ee3467b9f4462a
junbangg <wnstjr4620@naver.com>
2021-10-17 16:38:15 +0900

    refactor: DataChartViewController 코드 convention대로 수정

--- Stocky/Controller/DataChartViewController.swift
+++ Stocky/Controller/DataChartViewController.swift
@@ -4,27 +4,24 @@
 //
 //  Created by Jun Bang on 2021/03/12.
 //
-
 import Foundation
 import UIKit
 import TinyConstraints
 import Charts
 import Combine
-
 /**
  UIViewController to present Chart
  */
-class DataChartViewController : UIViewController, ChartViewDelegate {
-    
-    var timeSeries : TimeSeries?
-    var selectedIndex : Int?
-    
+class DataChartViewController: UIViewController, ChartViewDelegate {
+    var timeSeries: TimeSeries?
+    var selectedIndex: Int?
     //MARK: - Linechart View
     /// Creates Linechartview
-    lazy var lineChartView : LineChartView = {
+    lazy var lineChartView: LineChartView = {
         let chartView = LineChartView()
         chartView.backgroundColor = .themeGreenShade
         chartView.rightAxis.enabled = false
+        
         let yAxis = chartView.leftAxis
         yAxis.labelFont = .boldSystemFont(ofSize: 15)
         yAxis.setLabelCount(4, force: false)
@@ -39,12 +36,10 @@ class DataChartViewController : UIViewController, ChartViewDelegate {
         chartView.xAxis.labelTextColor = .white
         chartView.xAxis.labelPosition = .bottom
         chartView.xAxis.axisLineColor = .white
-        
         chartView.animate(xAxisDuration: 2.5)
         
         return chartView
     }()
-    
     //MARK: - viewDidLoad
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -54,20 +49,18 @@ class DataChartViewController : UIViewController, ChartViewDelegate {
         lineChartView.heightToWidth(of: view)
         setData()
     }
-    
     /// New Experimental Code for dynamic chart
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
-//        if selectedIndex != nil {
-//            setData()
-//            lineChartView.notifyDataSetChanged()
-//        }
+        //        if selectedIndex != nil {
+        //            setData()
+        //            lineChartView.notifyDataSetChanged()
+        //        }
     }
-    
     //MARK: - TODOS:
     /*
-        - figure out how to change the position of highlightIndicator according to the dateSlider index
-        - maybe use .circleColors() to indicate the highest price as a different color circle than the rest
+     - figure out how to change the position of highlightIndicator according to the dateSlider index
+     - maybe use .circleColors() to indicate the highest price as a different color circle than the rest
      */
     //MARK: - Set data
     /// Sets charts data and style configurations
@@ -75,10 +68,10 @@ class DataChartViewController : UIViewController, ChartViewDelegate {
         let priceData = LineChartDataSet(entries: fetchData(), label: "수정종가")
         
         priceData.mode = .cubicBezier
-//        priceData.drawCirclesEnabled = false
+        //        priceData.drawCirclesEnabled = false
         priceData.circleRadius = 3
         
-//        priceData.circleColors = .init([.systemBlue, .systemRed, .white])
+        //        priceData.circleColors = .init([.systemBlue, .systemRed, .white])
         priceData.lineWidth = 3
         priceData.setColor(.white)
         priceData.fill = Fill(color: .white)
@@ -91,46 +84,45 @@ class DataChartViewController : UIViewController, ChartViewDelegate {
     //MARK: - get data
     /// Fetches data from timeSeries and configures to [ChartDataEntry]
     private func fetchData() -> [ChartDataEntry]{
-        let monthData : [MonthData] = timeSeries?.getMonthData(isReversed: false) ?? []
-        var chartValues : [ChartDataEntry] = []
+        let monthData: [MonthData] = timeSeries?.getMonthData(isReversed: false) ?? []
+        var chartValues: [ChartDataEntry] = []
         var x = 0
         /// New experimental code
-//        guard let startIndex = selectedIndex else { return chartValues }
-//        let endIndex = monthData.count
-//        for index in startIndex...endIndex {
-//            let priceData = monthData[index]
-//            let yData = priceData.adjustedClose
-//            let data = priceData.date
-//            let chartData = ChartDataEntry(x: x.doubleValue, y: yData, data: data)
-//            chartValues.append(chartData)
-//            x += 1
-//        }
+        //        guard let startIndex = selectedIndex else { return chartValues }
+        //        let endIndex = monthData.count
+        //        for index in startIndex...endIndex {
+        //            let priceData = monthData[index]
+        //            let yData = priceData.adjustedClose
+        //            let data = priceData.date
+        //            let chartData = ChartDataEntry(x: x.doubleValue, y: yData, data: data)
+        //            chartValues.append(chartData)
+        //            x += 1
+        //        }
         ///Working original code
         for data in monthData {
             let yData = data.adjustedClose
             let data = data.date
-//            let chartData = ChartDataEntry(x: data, y: yData)
+            //            let chartData = ChartDataEntry(x: data, y: yData)
             let chartData = ChartDataEntry(x: x.doubleValue, y: yData, data: data)
             chartValues.append(chartData)
             x += 1
         }
         return chartValues
     }
-    
 }
 /// Class that is used for configuring Xaxis labels
 //https://stackoverflow.com/questions/54915102/trying-to-enter-date-string-in-chartdataentry
-class DateAxisValueFormatter : NSObject, IAxisValueFormatter {
-  let dateFormatter = DateFormatter()
-
-  override init() {
-    super.init()
-    dateFormatter.dateFormat = "dd MMM"
-  }
-
-  func stringForValue(_ value: Double, axis: AxisBase?) -> String {
-    let secondsPerDay = 24.0 * 3600.0
-    let date = Date(timeIntervalSince1970: value * secondsPerDay)
-    return dateFormatter.string(from: date)
-  }
+class DateAxisValueFormatter: NSObject, IAxisValueFormatter {
+    let dateFormatter = DateFormatter()
+    
+    override init() {
+        super.init()
+        dateFormatter.dateFormat = "dd MMM"
+    }
+    
+    func stringForValue(_ value: Double, axis: AxisBase?) -> String {
+        let secondsPerDay = 24.0 * 3600.0
+        let date = Date(timeIntervalSince1970: value * secondsPerDay)
+        return dateFormatter.string(from: date)
+    }
 }
