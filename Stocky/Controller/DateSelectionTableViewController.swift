bd6df058fdcfe916b332cb75875fc16e72bef692
junbangg <wnstjr4620@naver.com>
2021-10-17 16:36:19 +0900

    refactor: DateSelectionTableViewController 코드 convention대로 수정

--- Stocky/Controller/DateSelectionTableViewController.swift
+++ Stocky/Controller/DateSelectionTableViewController.swift
@@ -9,15 +9,13 @@ import UIKit
 
 /// UITableViewController for date selection
 
-class DateSelectionTableViewController : UITableViewController {
+class DateSelectionTableViewController: UITableViewController {
     //MARK: -  Properties
-    var timeSeries : TimeSeries?
-    var selectedIndex : Int?
-    private var monthDatas : [MonthData] = []
-    
+    var timeSeries: TimeSeries?
+    var selectedIndex: Int?
+    private var monthDatas: [MonthData] = []
     ///Closure that is passed to CalculatorTableViewController with the selected date index
     var didSelectDate: ((Int) -> Void)?
-    
     //MARK: - viewDidLoad
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -33,18 +31,14 @@ class DateSelectionTableViewController : UITableViewController {
         monthDatas = timeSeries?.getMonthData(isReversed: true) ?? []
     }
 }
-
 //MARK: - Extensions
-
 extension DateSelectionTableViewController {
-    
     /// Configure number of rows in a given section
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return monthDatas.count
     }
     /// Create cells configured with data
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        
         let cell = tableView.dequeueReusableCell(withIdentifier: "cellID", for: indexPath) as! DateSelectionTableViewCell
         let monthData = monthDatas[indexPath.item]
         let index = indexPath.item
@@ -59,15 +53,12 @@ extension DateSelectionTableViewController {
         tableView.deselectRow(at: indexPath, animated: true)
     }
 }
-
 /// Custom Tableview cell
-class DateSelectionTableViewCell : UITableViewCell {
-    
-    @IBOutlet weak var monthLabel : UILabel!
-    @IBOutlet weak var monthsAgoLabel : UILabel!
+class DateSelectionTableViewCell: UITableViewCell {
+    @IBOutlet weak var monthLabel: UILabel!
+    @IBOutlet weak var monthsAgoLabel: UILabel!
     
     func configure(with monthData: MonthData, with index: Int, isSelected: Bool) {
-        
         monthLabel.text = monthData.date.MMYYFormat
         /// Checkmark appears on selected cell
         accessoryType = isSelected ? .checkmark : .none
