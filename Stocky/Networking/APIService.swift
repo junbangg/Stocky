124f64428b71618bfde644800ceb2cecc4e67003
junbangg <wnstjr4620@naver.com>
2021-10-17 16:24:22 +0900

    refactor: APIService 코드 convention대로 수정

--- Stocky/Networking/APIService.swift
+++ Stocky/Networking/APIService.swift
@@ -8,7 +8,6 @@
 /// Networking
 import Foundation
 import Combine
-
 /**
  Struct used for networking with API
  
@@ -22,23 +21,21 @@ import Combine
  - private func pareseURL(): Used to format url string to URL type
  */
 struct APIService {
-    
     /// Error enum type
     enum APIServiceError: Error {
         case encoding
         case badRequest
     }
     /// Variable that will choose a random key from apiKeys
-    var API_KEY : String {
+    var API_KEY: String {
         return apiKeys.randomElement() ?? ""
     }
     /// Array of valid API keys
     let apiKeys = ["VWG848WN4TOAHX1P", "R4QEF20WS1UNXOP2", "3YVKJCWZ41BU608T", "Y9PIZ80TZ0XV3HVA"]
-    
     /// Function used to network with API to fetch data to present in SearchTableViewContoller when user inputs search query
     /// - Parameter key : User search query
     /// - Returns: AnyPublisher<SearchResults, Error>
-    func fetchPreviewData(with key : String) -> AnyPublisher<SearchResults, Error> {
+    func fetchPreviewData(with key: String) -> AnyPublisher<SearchResults, Error> {
         
         /// Parses key into "keywords" that will be used in networking URL string
         let keyParseResult = parse(query: key)
@@ -95,8 +92,6 @@ struct APIService {
             return Fail(error: error).eraseToAnyPublisher()
         }
     }
-    
-    
     /// Function to parse text into string usable in URL String
     /// - Parameter query: text to parse
     /// - Returns: Result<String, Error>
@@ -108,7 +103,6 @@ struct APIService {
             return .failure(APIServiceError.encoding)
         }
     }
-    
     /// Function to parse urlstring into URL Format
     /// - Parameter urlString: original urlString
     /// - Returns: Result<URL, Error>
