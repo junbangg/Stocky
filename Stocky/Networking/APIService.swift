bde413986a415e9f218914ff91597fee6832353b
junbangg <wnstjr4620@naver.com>
2022-01-03 14:25:00 +0900

    REFACTOR: APIService 클래스로 변경 및 프로토콜 분리

--- Stocky/Networking/APIService.swift
+++ Stocky/Networking/APIService.swift
@@ -7,37 +7,24 @@
 import Foundation
 import Combine
 
-struct APIService {
-    private enum APIServiceError: Error {
-        case encoding
-        case badRequest
-    }
+//MARK: API 요청 프로토콜
+protocol APIRequestable {
+    func fetchPreviewData(with key: String) -> AnyPublisher<SearchResults, Error>
+    func fetchTimeSeriesData(with key: String) -> AnyPublisher<TimeSeries, Error>
     
-    private enum APIServiceOperation {
-        private enum APIServiceURL {
-            static let baseString = "https://www.alphavantage.co/query?function="
-            static let symbolSearchString = "SYMBOL_SEARCH&keywords="
-            static let timeSeriesMonthlyAdjustedString = "TIME_SERIES_MONTHLY_ADJUSTED&symbol="
-            static let apiKeyString = "&apikey="
-            static let apiKeys = ["VWG848WN4TOAHX1P", "R4QEF20WS1UNXOP2", "3YVKJCWZ41BU608T", "Y9PIZ80TZ0XV3HVA"]
-            static var API_KEY: String {
-                return apiKeys.randomElement() ?? ""
-            }
-        }
-        
-        case symbolSearch(String)
-        case timeSeriesMonthlyAdjusted(String)
-        
-        var urlString: String {
-            switch self {
-            case .symbolSearch(let keywords):
-                return APIServiceURL.baseString + APIServiceURL.symbolSearchString + keywords + APIServiceURL.apiKeyString + APIServiceURL.API_KEY
-            case .timeSeriesMonthlyAdjusted(let symbol):
-                return APIServiceURL.baseString + APIServiceURL.timeSeriesMonthlyAdjustedString + symbol + APIServiceURL.apiKeyString + APIServiceURL.API_KEY
-            }
-        }
-    }
+}
+
+//MARK: 메인 클래스
+class APIService {
+    private let session: URLSession
     
+    init(session: URLSession = .shared) {
+        self.session = session
+    }
+}
+
+//MARK: 프로토콜 채택
+extension APIService: APIRequestable {
     // TODO: keyParseResult 메서드로 묶어서 분리
     // TODO: urlstring 다른 방법으로 관리
     func fetchPreviewData(with key: String) -> AnyPublisher<SearchResults, Error> {
@@ -93,7 +80,43 @@ struct APIService {
             return Fail(error: error).eraseToAnyPublisher()
         }
     }
+}
+
+//MARK: 중첩 타입
+private extension APIService {
+    private enum APIServiceError: Error {
+        case encoding
+        case badRequest
+    }
     
+    private enum APIServiceOperation {
+        private enum APIServiceURL {
+            static let baseString = "https://www.alphavantage.co/query?function="
+            static let symbolSearchString = "SYMBOL_SEARCH&keywords="
+            static let timeSeriesMonthlyAdjustedString = "TIME_SERIES_MONTHLY_ADJUSTED&symbol="
+            static let apiKeyString = "&apikey="
+            static let apiKeys = ["VWG848WN4TOAHX1P", "R4QEF20WS1UNXOP2", "3YVKJCWZ41BU608T", "Y9PIZ80TZ0XV3HVA"]
+            static var API_KEY: String {
+                return apiKeys.randomElement() ?? ""
+            }
+        }
+        
+        case symbolSearch(String)
+        case timeSeriesMonthlyAdjusted(String)
+        
+        var urlString: String {
+            switch self {
+            case .symbolSearch(let keywords):
+                return APIServiceURL.baseString + APIServiceURL.symbolSearchString + keywords + APIServiceURL.apiKeyString + APIServiceURL.API_KEY
+            case .timeSeriesMonthlyAdjusted(let symbol):
+                return APIServiceURL.baseString + APIServiceURL.timeSeriesMonthlyAdjustedString + symbol + APIServiceURL.apiKeyString + APIServiceURL.API_KEY
+            }
+        }
+    }
+}
+
+//MARK: - Helper 메서드
+private extension APIService {
     private func parse(query: String) -> Result<String, Error> {
         if let query = query.addingPercentEncoding(withAllowedCharacters: .urlHostAllowed) {
             return .success(query)
