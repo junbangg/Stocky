9c4cfdf57448fe344dc07edf617cae9d30526a9c
junbangg <wnstjr4620@naver.com>
2022-04-22 12:58:49 +0900

    REFACTOR: TimeSeries.getMonthData 리팩토링

--- Stocky/Model/TimeSeries.swift
+++ Stocky/Model/TimeSeries.swift
@@ -24,20 +24,27 @@ struct TimeSeries: Decodable {
     func getMonthData(isReversed: Bool) -> [MonthData] {
         var monthDataArray: [MonthData] = []
         let sortedTimeSeries = isReversed ? timeSeries.sorted(by: {$0.key > $1.key}) : timeSeries.sorted(by: {$0.key < $1.key})
+        let dateFormatter = createDateFormatter()
         
         for (dateString, data) in sortedTimeSeries {
-            let dateFormatter = DateFormatter()
-            dateFormatter.dateFormat = "yyyy-MM-dd"
             guard let date = dateFormatter.date(from: dateString),
                   let adjustedOpen = calculateAdjustedOpen(with: data),
                   let adjustedClose = data.adjustedClose.convertToDouble() else {
                 return []
             }
             let monthData = MonthData(date: date, adjustedOpen: adjustedOpen, adjustedClose: adjustedClose)
+            
             monthDataArray.append(monthData)
         }
-        
         return monthDataArray
+    }
+    
+    private func createDateFormatter() -> DateFormatter {
+        let dateFormatter = DateFormatter()
+        
+        dateFormatter.dateFormat = "yyyy-MM-dd"
+        
+        return dateFormatter
     }
     
     private func calculateAdjustedOpen(with timeSeriesData: TimeSeriesData) -> Double? {
@@ -46,7 +53,6 @@ struct TimeSeries: Decodable {
               let close = timeSeriesData.close.convertToDouble() else {
             return nil
         }
-
         return open * adjustedClose / close
     }
 }
